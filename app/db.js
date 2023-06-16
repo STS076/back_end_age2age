fb7c45c2abcac8e115debbc1f0fc1108f2de9cd9
NekShor <Antoine27bigot@gmail.com>
2023-06-16 15:26:29 +0200

    test bad number

--- app/db.js
+++ app/db.js
@@ -1,41 +1,50 @@
 // const mysql = require("mysql");
-// const dbConfig = require("./config/db.config.json");
 
 const mysql = require('mysql2/promise');
 const { Sequelize } = require('sequelize');
 
 module.exports = db = {};
 
-// initialize();
+initialize();
 
-// async function initialize() {
-//   // create db if it doesn't already exist
-//   const { host, port, user, password, database } = dbConfig.database;
-//   const connection = await mysql.createConnection({ host, port, user, password });
-//   await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+async function initialize() {
+  var dbConfig = null
+  try{
+    dbConfig = require("./config/db.config.json");
+  }catch(e){
+  }
 
-//   // connect to db
-//   const sequelize = new Sequelize(database, user, password, {
-//     dialect: 'mysql', define: {
-//       timestamps: false,
-//       createdAt: false,
-//       updatedAt: false,
-//       UserUserId: false,
-//     },
-//     port: port
-//   });
+  if(dbConfig){
+  // create db if it doesn't already exist
+  const { host, port, user, password, database } = dbConfig.database;
+  const connection = await mysql.createConnection({ host, port, user, password });
+  await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
-//   // init models and add them to the exported db object
-//   db.User = require('./model/Users')(sequelize);
-//   db.Advert = require('./model/Adverts')(sequelize);
-//   db.Roles = require('./model/Roles')(sequelize);
-//   db.Categories = require('./model/Categories')(sequelize);
-//   db.Comments = require('./model/Comments')(sequelize);
-//   db.Messages = require('./model/Messages')(sequelize);
-//   db.User_has_favourite = require('./model/UserHasFavourite')(sequelize);
+  // connect to db
+  const sequelize = new Sequelize(database, user, password, {
+    dialect: 'mysql', define: {
+      timestamps: false,
+      createdAt: false,
+      updatedAt: false,
+      UserUserId: false,
+    },
+    port: port
+  });
+
+  // init models and add them to the exported db object
+  db.User = require('./model/Users')(sequelize);
+  db.Advert = require('./model/Adverts')(sequelize);
+  db.Roles = require('./model/Roles')(sequelize);
+  db.Categories = require('./model/Categories')(sequelize);
+  db.Comments = require('./model/Comments')(sequelize);
+  db.Messages = require('./model/Messages')(sequelize);
+  db.User_has_favourite = require('./model/UserHasFavourite')(sequelize);
 
 
 
-//   // sync all models with database
-//   await sequelize.sync();
-// }
+  // sync all models with database
+  await sequelize.sync();
+  }else{
+    console.log("no db config")
+  }
+}
