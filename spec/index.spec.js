1fa46a7ccd11afa91d19446a5c8dc97ce63ed778
NekShor <Antoine27bigot@gmail.com>
2023-08-23 16:03:23 +0200

    Test unitaires

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -2,6 +2,7 @@ const request = require('request');
 const server = require('server');
 const User = require('../app/service/user.service');
 const Role = require('../app/service/role.service');
+const Advert = require('../app/service/advert.service');
 const Sequelize = require('sequelize');
 const sqlite = require('sqlite3');
 const db = require('../app/db');
@@ -11,6 +12,12 @@ initialize();
 async function initialize() {
     await new Promise(resolve => setTimeout(() => resolve(), 500));
     await db.Roles.create({role_type: 'user'})
+    await db.Roles.create({role_type: 'modérateur'})
+    await db.Roles.create({role_type: 'admin'})
+
+    await db.Categories.create({category_type: 'Véhicules'})
+    await db.Categories.create({category_type: 'Immobilier'})
+    await db.Categories.create({category_type: 'Multimédia'})
 }
 
 
@@ -40,56 +47,185 @@ describe('check email test', function () {
     });
 });
 
+(async () => {
 
-let pseudo = "totototo"
-
-describe('User Routes', () => {
-    beforeAll(async () => {
-        await new Promise(resolve => setTimeout(() => resolve(), 3000));
-
-        try{
-            await db.User.create({ 
-                user_email_address: 'totototo@gmail.com',
-                user_phone_number: '0606060606',
-                user_pseudo: pseudo,
-                user_password: 'jksdrgjebzrb',
-                user_firstname: 'toto',
-                user_lastname: 'tota',
-                user_address: '10 rue Francois Arago',
-                user_zip_code: '93100',
-                user_city: 'Montreuil',
-                user_department: '93',
-                role_id: 1,
-                user_active: 1,
-            }); // Insérer un utilisateur de test
-
-        } catch(e){
-            console.log(e)
-        }
-
-    });
-
-    it('should get a user by user pseudo', async () => {
-        var allUsers = await User.getAll();
-        var find = false;
-
-        allUsers = JSON.parse(JSON.stringify(allUsers));
-        
-        for (var i = 0; i < allUsers.length; i++) {
-            if (allUsers[i].user_pseudo == pseudo) {
-                find = true;
+    let pseudo = "totototo"
+    let roleExpected = 2;
+    var idUser = null;
+    describe('test Users', () => {
+        beforeAll(async () => {
+            await new Promise(resolve => setTimeout(() => resolve(), 3000));
+    
+            try{
+                await db.User.create({ 
+                    user_email_address: 'totototo@gmail.com',
+                    user_phone_number: '0606060606',
+                    user_pseudo: pseudo,
+                    user_password: 'jksdrgjebzrb',
+                    user_firstname: 'toto',
+                    user_lastname: 'tota',
+                    user_address: '10 rue Francois Arago',
+                    user_zip_code: '93100',
+                    user_city: 'Montreuil',
+                    user_department: '93',
+                    role_id: 1,
+                    user_active: 1,
+                }); 
+    
+            } catch(e){
+                console.log(e)
             }
-        }
+    
+        });
 
-        expect(find).toBe(true);
+    
+        it('Créer un utilisateur', async () => {
+            
+            var allUsers = await User.getAll();
+            var find = false;
+    
+            allUsers = JSON.parse(JSON.stringify(allUsers));
+            
+            for (var i = 0; i < allUsers.length; i++) {
+                if (allUsers[i].user_pseudo == pseudo) {
+                    find = true;
+                    idUser = allUsers[i].user_id;
+                }
+            }
+    
+            expect(find).toBe(true);
+        });
+
+        it('Changer le role', async () => {
+            await new Promise(resolve => setTimeout(() => resolve(), 400));
+
+            if(idUser == null){
+                idUser = 1;
+            }
+            await User.updateUserRole(idUser, roleExpected);
+            var user = await User.getById(idUser);
+            user = user[0]
+            expect(user.role_id).toBe(roleExpected);
+            
+        });
+    
+        afterAll(async () => {
+        });
+      
     });
 
-    afterAll(async () => {
+
+    describe('test Adverts', () => {
+        let title = "test"
+        let description = "test2"
+        var advertId = null;
+        beforeAll(async () => {
+            await new Promise(resolve => setTimeout(() => resolve(), 3000));
+    
+            try{
+                await db.Advert.create({ 
+                    advert_title: title,
+                    advert_description: 'test1',
+                    advert_active: 1,
+                    advert_archive: 0,
+                    advert_selected: 0,
+                    advert_address: '10 rue Francois Arago',
+                    advert_zip_code: '93100',
+                    advert_city: 'Montreuil',
+                    advert_department: '93',
+                    advert_longitude: 0,
+                    advert_latitude: 0,
+                    category_id: 1,
+                    user_id_create: 1,
+                    user_id_select: 1,
+
+                }); 
+    
+            } catch(e){
+                console.log(e)
+            }
+    
+        });
+
+    
+        it('Créer une annonce', async () => {
+            await new Promise(resolve => setTimeout(() => resolve(), 400));
+            
+            var allAdvert = await Advert.findByTitle(title);
+
+            expect(allAdvert.length > 0).toBe(true);
+        });
+
+        it('modifier une annonce', async () => {
+            await new Promise(resolve => setTimeout(() => resolve(), 800));
+
+            if(advertId == null){
+                advertId = 1;
+            }
+            var advertChange = await Advert.findByTitle(title);
+            var idAdvert = advertChange[0].advert_id;
+
+            await Advert.update(idAdvert, {advert_description: description});
+
+            var advertChange = await Advert.findByTitle(title);
+            advertChange = advertChange[0]
+
+            expect(advertChange.advert_description).toBe(description);
+            
+        });
+
+        afterAll(async () => {
+        });
+      
     });
-  
-});
-  
-  
+
+
+
+
+    describe('test roles', () => {
+        let title = "Jouets"
+
+        beforeAll(async () => {
+            await new Promise(resolve => setTimeout(() => resolve(), 3000));
+    
+            try{
+                await db.Roles.create({ 
+                    role_type: title,
+
+                });
+    
+            } catch(e){
+                console.log(e)
+            }
+    
+        });
+
+    
+        it('Créer une annonce', async () => {
+            await new Promise(resolve => setTimeout(() => resolve(), 400));
+            
+            var allRoles = await Role.findAll();
+
+            allRoles = JSON.parse(JSON.stringify(allRoles));
+            var find = false;
+
+            for (var i = 0; i < allRoles.length; i++) {
+                if (allRoles[i].role_type == title) {
+                    find = true;
+                }
+            }
+
+            expect(find).toBe(true);
+
+        });
+
+        afterAll(async () => {
+        });
+      
+    });
+    
+      
+})();
   
   
   
